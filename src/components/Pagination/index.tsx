d14ac93c2f012a2bee65dd801257faa556734cee
Lucas Delbel <lucdelbel@gmail.com>
2021-04-28 11:36:49 -0300

    Create pagination structure

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,44 @@
-import { Stack, Button, Box } from "@chakra-ui/react";
+import { Stack, Text, Box } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange?: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+} // generatePagesArray(2,5) => [3,4,5]
+
+export function Pagination({
+  totalCountOfRegisters,
+  currentPage,
+  registersPerPage = 10,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -14,10 +51,31 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> of <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />              
+        {currentPage > 1 + siblingsCount &&( 
+          <>
+          <PaginationItem number={1} />
+          { currentPage > (2 + siblingsCount) && <Text color="gray.300" width="8" textAlign="center">...</Text>}
+          </>
+          )}
+
+        {previousPages.length > 0 &&
+          previousPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        <PaginationItem number={currentPage} isCurrent />
+
+        {nextPages.length > 0 &&
+          nextPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        {(currentPage + siblingsCount ) < lastPage && (
+        <>
+          { (currentPage + 1 + siblingsCount) < lastPage && <Text color="gray.300" width="8" textAlign="center">...</Text>}
+          <PaginationItem number={lastPage} />
+        </>
+        )}
       </Stack>
     </Stack>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -113,7 +113,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination 
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
